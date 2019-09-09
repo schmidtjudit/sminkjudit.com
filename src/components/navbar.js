f7563cf9741dec1d20d6dc306896b6eb0201134d
Johan Guttormsson <johan@jgsoft.se>
2019-09-09 13:33:19 +0200

    add avatar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,14 +1,34 @@
 import React from "react"
+
+import { Link, useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+import "./navbar.css"
 import { SocialInstagram } from "styled-icons/typicons"
 
-import { Link } from "gatsby"
-import "./navbar.css"
-
 const Navbar = () => {
+  const avatar = useStaticQuery(graphql`
+    query GetAvatarFile {
+      file(relativePath: { eq: "avatar.jpg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  console.log(avatar)
+
   return (
     <aside className="nav">
       <div>
         <Link to="/">
+          <Img
+            className="avatar"
+            fluid={avatar.file.childImageSharp.fluid}
+            alt="Avatar"
+          />
           <h1>Judit Schmidt</h1>
           <h2>Sikkes - Sminkes - Sikeres</h2>
         </Link>
