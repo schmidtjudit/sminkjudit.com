863e07de0ee29271de2ac86bae65a44c90d33d4e
Johan Guttormsson <johan@jgsoft.se>
2019-09-11 11:06:15 +0200

    fix gallery style

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -2,18 +2,26 @@ import React from "react"
 import Layout from "../components/layout"
 import Portfolio from "../components/portfolio"
 import SEO from "../components/seo"
+import styled from "styled-components"
+
+const GalleryPage = styled.div`
+  min-width: 97vw;
+  @media (min-width: 768px) {
+    min-width: 62vw;
+  }
+`
 
 const PortfolioPage = () => (
   <Layout>
     <SEO title="Portfólió" />
-    <div className="article">
+    <GalleryPage className="article">
       <header>
         <h1>Portfólió</h1>
       </header>
       <section>
         <Portfolio />
       </section>
-    </div>
+    </GalleryPage>
   </Layout>
 )
 
